61daa8f21028cac05132834b9c9d928f81e45354
tanisk singh <159751627+TaniskRajput@users.noreply.github.com>
2026-04-28 01:06:34 +0530

    Patterns:Square Pattern done

---
+++ CPP/patterns/square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row=3;
+    int column=3;
+    for(int i =0;i<row;i++){
+        for(int j=0;j<column;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
