c03a4799eccda182b78f0f796929b2faaea17abc
tanisk singh <159751627+TaniskRajput@users.noreply.github.com>
2026-04-29 00:13:32 +0530

    code:Pyramid pattern

---
+++ CPP/patterns/pyramid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size=5;
+    for(int i = 1;i<=size;i++){
+        for(int j=1;j<=size-i;j++){
+            cout<<" ";
+        }
+        for(int j = 1 ;j<=2*i-1;j++){
+            cout<<"*";
+        }     
+        cout<<endl; 
+    }
+}
