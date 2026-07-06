a9941040621619fa0117a6465075b9c57dba6855
tanisk singh <tanisk12357@gmail.com>
2026-07-06 15:33:53 +0530

    Add webd project with HTML, CSS, and JavaScript files

---
+++ Data/webd/javascript/1.js
@@ -0,0 +1,7 @@
+let bookings = 1;
+let coupon = "Hello";
+if (bookings == 0 && coupon === "Hello") {
+    console.log("You can use the coupon");
+} else {
+    console.log("You cannot use the coupon");
+}
---
+++ Data/webd/javascript/script.js
@@ -0,0 +1,16 @@
+// let x = 10
+// x = "tanisk"
+// console.log(x)
+// let x;
+// console.log(x)
+// let x = null
+// console.log(x)
+// let boolean = true
+// console.log(boolean)
+console.log("Tanisk" || "hello world")
+console.log("" || "hello world")
+console.log([] && "hello")//empty array is true
+console.log({} && "hello")//empty object is true
+console.log(0 && "hello")//0 is false
+console.log(null && "hello")//null is false
+console.log(undefined && "hello")//undefined is false
